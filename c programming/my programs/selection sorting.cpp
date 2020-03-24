d77e3d408c41c9d1fd1cd6e704b60dd5d5b3264d
Luv Kumar <51437281+Luv8436@users.noreply.github.com>
2020-03-24 14:05:49 +0530

    Add files via upload
    sorting programs in c

---
+++ c programming/my programs/selection sorting.cpp
@@ -0,0 +1,41 @@
+// Program for selection sorting 
+#include<stdio.h>
+int main()
+{
+	int n ;
+	printf("enter the number of elements in array: ");
+	scanf("%d",&n);
+	printf("enter the %d elements: ",n);
+	int arr[n];
+	for(int i = 0 ; i < n ; i++)
+	{
+		scanf("%d",&arr[i]);
+	} 
+	// outer loop 
+	for(int i = 0 ; i < n - 1 ; i++)
+	{
+		// taking small variable to arr[i]
+		int small = arr[i];
+		int index = i;
+		int flag = 0;
+		// inner loop
+		for(int j = i + 1 ; j < n ; j++)
+		{
+			if(small > arr[j])
+			{
+				small = arr[j];
+				index = j;
+			}
+		}
+		int temp = arr[i];
+		arr[i] = small;
+		arr[index] = temp;
+	}
+	
+	// print the loop
+	for(int i = 0 ; i < n ; i++)
+	{
+		printf("%d ",arr[i]);
+	}
+}
+
